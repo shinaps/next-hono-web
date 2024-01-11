1cc8baf692728b13ee61a09031d8c2620be8fe19
shinaps <axon@shinaps.jp>
2024-01-12 00:27:37 +0900

    create static ui

---
+++ src/components/main-nav.tsx
@@ -0,0 +1,38 @@
+import Link from 'next/link'
+
+import { cn } from '@/lib/utils'
+import { HTMLAttributes } from 'react'
+
+export function MainNav({ className, ...props }: HTMLAttributes<HTMLElement>) {
+  return (
+    <nav
+      className={cn('flex items-center space-x-4 lg:space-x-6', className)}
+      {...props}
+    >
+      <Link
+        href='#'
+        className='text-sm font-medium transition-colors hover:text-primary'
+      >
+        Overview
+      </Link>
+      <Link
+        href='#'
+        className='text-sm font-medium text-muted-foreground transition-colors hover:text-primary'
+      >
+        Customers
+      </Link>
+      <Link
+        href='#'
+        className='text-sm font-medium text-muted-foreground transition-colors hover:text-primary'
+      >
+        Products
+      </Link>
+      <Link
+        href='#'
+        className='text-sm font-medium text-muted-foreground transition-colors hover:text-primary'
+      >
+        Settings
+      </Link>
+    </nav>
+  )
+}
